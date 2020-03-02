fd462be33df21306f1d1cb6bab2497797921c82a
Utkarsh Shrivastav <utkarsh.sri90@gmail.com>
2020-03-02 16:24:05 +0530

    comment module made

---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,19 @@
+module Api
+    module V1
+        class CommentsController < ApplicationController
+            def index
+                comments = Comment.all
+                render json: comments
+              end
+            def create
+                comment = Comment.create(comment_params)
+                comment.save
+                render json: comment
+            end
+            private
+            def comment_params
+                params.require(:comment).permit(:comment, :likes, :user_id, :message_id)
+            end
+        end
+    end
+end
--- app/javascript/components/Message.js
+++ app/javascript/components/Message.js
@@ -1,15 +1,13 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { Card, Button } from "semantic-ui-react"
-import { CreateComment } from './CreateComment'
-import { Redirect, Switch } from 'react-router-dom'
-import {Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import { Card, Button,Header, Icon, Comment, Form, Modal } from "semantic-ui-react"
 export class Message extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       editable: false,
-      isCreateComment: false
+      isCreateComment: false,
+      comments: {}
     }
     this.handleEdit = this.handleEdit.bind(this)
   }
@@ -25,19 +23,6 @@ export class Message extends React.Component {
       editable: !this.state.editable
     })
   }
-  callCreateComment = () => {
-    console.log('routing');
-    <Router>
-      <ul>
-        <li>
-          <Link to="/CreateComment">CreateComment</Link>
-        </li>
-      </ul>
-      <Switch>
-      <Route exact path="/CreateComment" component={CreateComment}/>
-      </Switch>
-    </Router>
-  }
   render() {
     let title = this.state.editable ?
       <input type="text" ref={input => this.title = input} defaultValue={this.props.message.title} /> :
@@ -47,25 +32,55 @@ export class Message extends React.Component {
       <h4>{this.props.message.message}</h4>
     return (
       <React.Fragment>
-            <Card.Content>
-              {title}
-            </Card.Content>
-            <Card.Content>
-              {message}
-            </Card.Content>
-            <Card.Content extra>
-              <div className='ui two buttons'>
-                <Button onClick={() => this.handleEdit()} basic color='green'>{this.state.editable ? 'Submit' : 'Edit'}</Button>
-                <Button onClick={() => this.props.handleDelete(this.props.message.id)} basic color='red'>
-                  Delete
+        <Card.Content>
+          {title}
+        </Card.Content>
+        <Card.Content>
+          {message}
+        </Card.Content>
+        <Card.Content extra>
+          <div className='ui two buttons'>
+            <Button onClick={() => this.handleEdit()} basic color='green'>{this.state.editable ? 'Submit' : 'Edit'}</Button>
+            <Button onClick={() => this.props.handleDelete(this.props.message.id)} basic color='red'>
+              Delete
               </Button>
-                {/* <Button basic color='blue'>Comment</Button> */}
-              </div>
-            </Card.Content>
-            <Card.Content>
-            <Button basic color='blue' onClick={this.callCreateComment}>Comment</Button>
-              {/* <Button basic color='blue' onClick={this.callCreateComment}>Comment</Button> */}
-            </Card.Content>
+            {/* <Button basic color='blue'>Comment</Button> */}
+          </div>
+        </Card.Content>
+        <Modal trigger={<Button>Comment</Button>} closeIcon>
+         <Modal.Header>{this.props.message.title}</Modal.Header>
+          <Modal.Content image scrolling>
+              {/* <Header>Modal Header</Header> */}
+              <p>{this.props.message.message}</p>
+
+              {/* {_.times(8, (i) => (
+                <Image
+                  key={i}
+                  src='https://react.semantic-ui.com/images/wireframe/paragraph.png'
+                  style={{ paddingBottom: 5 }}
+                />
+              ))} */}
+            
+          </Modal.Content>
+          <Modal.Content>
+          <Comment.Group>
+              <Header as='h4' dividing>Comment</Header>
+              <Comment>
+                <Comment.Author>Author Name</Comment.Author>
+                <Comment.Text>Nice comment</Comment.Text>
+              </Comment>
+              <Form reply>
+                {/* <Form.TextArea /> */}
+                <textarea />
+              </Form>
+            </Comment.Group>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button primary onClick={this.handleClick}>
+              Post Comment <Icon name='chevron right' />
+            </Button>
+          </Modal.Actions>
+        </Modal>
       </React.Fragment>
     );
   }
