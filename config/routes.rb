8dabaf831a433ce97d6a07bd7b5f7a21d73e0262
Utkarsh Shrivastav <utkarsh.sri90@gmail.com>
2020-03-03 17:59:01 +0530

    added routes for user and added bcrypt gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/signup' => "messages#signup"
   namespace :api do
     namespace :v1 do
+      resources :users
       resources :messages, only: [:index, :create, :destroy, :update]
       resources :comments, only: [:index, :create]
     end
